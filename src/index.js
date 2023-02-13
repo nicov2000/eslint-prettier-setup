e76689ccd95670feb39da9d3e44497f8b8e350e5
nicov2000 <nicolasvarg00@gmail.com>
2023-02-12 22:19:33 -0300

    fix: fixed port variable

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import express from "express"
 const app = express()
 const port = process.env.port || 3000
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log(`Server listening on http://localhost:${port}`)
 })
